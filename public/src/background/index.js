e7e75d0a9760e6b53b05977dead411843e7fea9b
Michael Budnick <mdbudnick@users.noreply.github.com>
2025-06-07 12:57:15 -0400

    chore: delete public dir

--- public/src/background/index.js
+++
@@ -1,4 +0,0 @@
-// Minimal background script for Read and Scroll
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Read and Scroll extension installed.");
-});
--- public/src/content/index.js
+++
@@ -1,2 +0,0 @@
-// Minimal content script for Read and Scroll
-console.log("Read and Scroll content script loaded.");
