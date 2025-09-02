efa87da9565cd1f2b652aaf5d2227768ed57514f
Michael Budnick <mdbudnick@users.noreply.github.com>
2025-09-02 17:18:41 -0400

    Create wxt.config.ts and remove unused icon

--- wxt/wxt.config.ts
+++ wxt/wxt.config.ts
@@ -1,38 +1,39 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
   manifest: {
     name: "Read and Scroll",
     short_name: "ReadScroll",
     version: "1.0.2",
-    description: "An extension that adds a reader mode and auto-scroll to any website in Chrome.",
+    description:
+      "An extension that adds a reader mode and auto-scroll to any website in Chrome.",
     permissions: ["activeTab", "storage"],
     action: {
       default_icon: {
         "16": "icon/16.png",
-        "32": "icon/32.png", 
+        "32": "icon/32.png",
         "48": "icon/48.png",
-        "128": "icon/128.png"
-      }
+        "128": "icon/128.png",
+      },
     },
     icons: {
       "16": "icon/16.png",
       "32": "icon/32.png",
-      "48": "icon/48.png", 
-      "128": "icon/128.png"
+      "48": "icon/48.png",
+      "128": "icon/128.png",
     },
     web_accessible_resources: [
       {
         resources: [
           "icon/128.png",
-          "icon/48.png", 
+          "icon/48.png",
           "icon/32.png",
-          "icon/16.png"
+          "icon/16.png",
         ],
-        matches: ["<all_urls>"]
-      }
-    ]
-  }
+        matches: ["<all_urls>"],
+      },
+    ],
+  },
 });
